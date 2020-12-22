d709a3eaed7d609cdbabfe8c6f07cf6640aabab7
WaqasAkbarEngr <68653306+WaqasAkbarEngr@users.noreply.github.com>
2020-12-22 13:42:19 +0500

    Add files via upload

---
+++ Ultrasonic_Trial/Ultrasonic_Trial.ino
@@ -0,0 +1,40 @@
+// initializing pins of ultrasonic sensor
+#define trigger 10
+#define echo 9
+
+// initializing constant with value equal to speed of sound
+#define SPEED_OF_SOUND 340
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // Starting Serial Communication with baud rate 9600
+  Serial.begin(9600);
+
+  // setting input/output pins
+  pinMode(trigger,OUTPUT);
+  pinMode(echo,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // setting trigger pin to LOW in case it was already on
+  digitalWrite(trigger,LOW);
+  // delay of 2 microseconds to let pin attain a value of 0
+  delayMicroseconds(2);
+
+  // triggering trigger pin to HIGH state for 10 microseconds and then turning it back to LOW
+  digitalWrite(trigger,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger,LOW);
+
+  float timeTaken = pulseIn(echo,HIGH)/2;
+
+  float distance = (SPEED_OF_SOUND * timeTaken) / 1000;
+
+  Serial.print("Distance = ");
+  Serial.println(distance);
+
+}
